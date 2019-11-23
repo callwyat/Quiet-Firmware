d001c3329a064889ce8c61544442467727e63ce9
Wyatt Callister <callwyat@isu.edu>
2019-11-23 12:52:18 -0800

    Nuked the projec and started over from an MCC Base

--- code/app/Qy@/Communications/qI2C.h
+++
@@ -1,6 +0,0 @@
-/* 
- * File:   Setup.h
- * Author: Wyatt
- *
- * Created on May 21, 2018, 7:59 PM
- */
---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Quiet.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Quiet.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=quiet.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/quiet.x.tar
