1aca4b222b59f7470b3f2fafe95a28279fc18123
Wyatt Callister <callwyat@isu.edu>
2022-04-18 11:36:30 -0700

    Added error codes to the DIGI commands

--- src/Quiet.X/Commands/digiCommand.c
+++ src/Quiet.X/Commands/digiCommand.c
@@ -2,8 +2,6 @@
 #include "../CLI/cli.h"
 #include "../constants.h"
 
-#define DIGIN_CHNNEL ((uint8_t)(*((uint8_t*)channel)))
-
 void DIGIDiscreetCommand(CliBuffer_t *buffer, void* v) {
     if (*buffer->InputPnt == '?') {
         //Progress the pointer past the query
@@ -12,14 +10,23 @@ void DIGIDiscreetCommand(CliBuffer_t *buffer, void* v) {
     }
 }
 
-void DIGIChannelValueCommand(CliBuffer_t *buffer, void* channel) {
+void DIGIChannelValueCommand(CliBuffer_t *buffer, void* v) {
 
     if (*buffer->InputPnt == '?')
     {
         ++buffer->InputPnt;
-        uint8_t value = (DIN & (1 << ((DIGIN_CHNNEL) - 1))) > 0;
-        
-        NumberToString(buffer, value);
+
+        uint8_t channel = *((uint8_t*)v);
+
+        if (channel >= 1 && channel <= 8)
+        {
+            uint8_t value = (DIN & (1 << ((channel) - 1))) > 0;
+            NumberToString(buffer, value);
+        }
+        else
+        {
+            QueueErrorCode(DIGI_ERROR_INVALID_CHANNEL);
+        }
     }
 }
     
